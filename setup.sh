432b4395718e00bc328f3918ca6467d9021c093d
Mentor <mentor@palokaj.co>
2016-11-21 22:02:44 +0100

    Dependency update and user modularity

--- setup.sh
+++ setup.sh
@@ -1,10 +1,14 @@
 read -p "Server ip/domain: " serverip
 echo "Server set to $serverip"
+read -p "Server user: " serveruser
+echo "User set to $serveruser"
 read -p "Local ssh key path [default ~/.ssh/deploy.pub]: " sshkey
 sshkey=${sshkey:-$HOME/.ssh/deploy.pub}
 echo "SSH public key set to $sshkey"
 sshpub="$(<$sshkey)"
 rootsetup="
+sudo apt-get update
+sudp apt-get install -y git
 sudo useradd --create-home -s /bin/bash deploy
 sudo adduser deploy sudo
 sudo passwd deploy
@@ -32,9 +36,9 @@ echo "$rootsetup" > rootsetup.sh
 echo "$deploysetup" > deploysetup.sh
 
 echo "Copy root script to remote host"
-scp ./rootsetup.sh root@$serverip:~
+scp ./rootsetup.sh $serveruser@$serverip:~
 read -p "Connecting as root. Please run ~/rootsetup.sh script and exit when script completes."
-ssh root@$serverip
+ssh $serveruser@$serverip
 echo "Copy root script to remote host"
 scp ./deploysetup.sh deploy@$serverip:~
 read -p "Connecting as deploy. Please run ~/deploysetup.sh script and exit when script completes."
